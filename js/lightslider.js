95b1492f7641fea6f3302dce23ef27602ca122b2
Eric Prem Raju <58141370+ericpremraju@users.noreply.github.com>
2021-09-24 18:32:49 +0530

    Final layout(hopefully)

--- js/lightslider.js
+++ js/lightslider.js
@@ -15,7 +15,7 @@
         pauseOnHover: false,
         loop: false,
         slideEndAnimation: true,
-        pause: 2000,
+        pause: 5000,
         keyPress: false,
         controls: true,
         prevHtml: '',
