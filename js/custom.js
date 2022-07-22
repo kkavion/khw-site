56b4b90dc54afc98033696bf463165b2aabc6695
Eric Prem Raju <ericpremraju@gmail.com>
2022-07-22 19:05:37 +0530

    update

--- js/custom.js
+++ js/custom.js
@@ -410,3 +410,12 @@
     });
 
 })(jQuery);
+// Open the full screen search box
+function openSearch() {
+	document.getElementById("myOverlay").style.display = "block";
+  }
+  
+  // Close the full screen search box
+  function closeSearch() {
+	document.getElementById("myOverlay").style.display = "none";
+  } 
