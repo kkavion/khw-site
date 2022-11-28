cab49b027bab07f2a9c372deceb5ab0d46eddfda
Eric Prem Raju <ericpremraju@gmail.com>
2022-11-28 16:15:31 +0530

    added dayout rows in interest pages

---
+++ js/bid.js
@@ -0,0 +1,36 @@
+function bid1()
+{
+	document.forms["bidnwin"]["Property"].value = "Vedic Village";
+	document.forms["bidnwin"]["Bid Number"].value = "BD #101 - November 2022";
+	document.forms["bidnwin"]["Bid Amount"].min = "9999"
+}
+function bid2()
+{
+	document.forms["bidnwin"]["Property"].value = "The Amaya Resort";
+	document.forms["bidnwin"]["Bid Number"].value = "BD #102 - November 2022";
+	document.forms["bidnwin"]["Bid Amount"].min = "9999"
+}
+function bid3()
+{
+	document.forms["bidnwin"]["Property"].value = "The Ffort Raichak";
+	document.forms["bidnwin"]["Bid Number"].value = "BD #103 - November 2022";
+	document.forms["bidnwin"]["Bid Amount"].min = "9999"
+}
+function bid4()
+{
+	document.forms["bidnwin"]["Property"].value = "Fortune Park Panchawati";
+	document.forms["bidnwin"]["Bid Number"].value = "BD #104 - November 2022";
+	document.forms["bidnwin"]["Bid Amount"].min = "9999"
+}
+function bid5()
+{
+	document.forms["bidnwin"]["Property"].value = "The Rajbari Bawali";
+	document.forms["bidnwin"]["Bid Number"].value = "BD #105 - November 2022";
+	document.forms["bidnwin"]["Bid Amount"].min = "9999"
+}
+function bid6()
+{
+	document.forms["bidnwin"]["Property"].value = "Ibiza The Fern Resort & Spa";
+	document.forms["bidnwin"]["Bid Number"].value = "BD #106 - November 2022";
+	document.forms["bidnwin"]["Bid Amount"].min = "9999"
+}
--- js/custom.js
+++ js/custom.js
@@ -521,33 +521,3 @@ function closeRoomCompare()
 {
 	document.getElementById("room-compare-section").style.display = "none";
 }
-function bid1()
-{
-	document.forms["bidnwin"]["Property"].value = "Vedic Village";
-	document.forms["bidnwin"]["Bid Number"].value = "BD #101 - November 2022";
-}
-function bid2()
-{
-	document.forms["bidnwin"]["Property"].value = "The Amaya Resort";
-	document.forms["bidnwin"]["Bid Number"].value = "BD #102 - November 2022";
-}
-function bid3()
-{
-	document.forms["bidnwin"]["Property"].value = "The Ffort Raichak";
-	document.forms["bidnwin"]["Bid Number"].value = "BD #103 - November 2022";
-}
-function bid4()
-{
-	document.forms["bidnwin"]["Property"].value = "Fortune Park Panchawati";
-	document.forms["bidnwin"]["Bid Number"].value = "BD #104 - November 2022";
-}
-function bid5()
-{
-	document.forms["bidnwin"]["Property"].value = "The Rajbari Bawali";
-	document.forms["bidnwin"]["Bid Number"].value = "BD #105 - November 2022";
-}
-function bid6()
-{
-	document.forms["bidnwin"]["Property"].value = "Ibiza The Fern Resort & Spa";
-	document.forms["bidnwin"]["Bid Number"].value = "BD #106 - November 2022";
-}
