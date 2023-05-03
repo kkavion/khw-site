8500e0a5d11b257b2ee8930f805aa97de3895cad
Eric Prem Raju <58141370+ericpremraju@users.noreply.github.com>
2023-05-03 18:06:47 +0530

    added resort logos and some fixes

--- js/custom.js
+++ js/custom.js
@@ -5,14 +5,14 @@
 		$('#status').fadeOut();
 		$('#preloader').delay(100).fadeOut('fast');
 		$(window).stellar({});
-		//$('#myModal2').addClass("show");
+		$('#myModal2').addClass("show");
 	});
-	//$('#myModal2').show();
-	//$('#startModal').show();
-	//$("#myModal2 button").click(function() {
-	//	$('#myModal2').fadeOut("slow");
-	//	$('#startModal').fadeOut("slow");
-	//});
+	$('#myModal2').show();
+	$('#startModal').show();
+	$("#myModal2 button").click(function() {
+		$('#myModal2').fadeOut("slow");
+		$('#startModal').fadeOut("slow");
+	});
 
 	/*====== countdown ========*/
 	$('[data-countdown]').each(function () {
@@ -419,52 +419,8 @@
 
 
 
-// Create cookie
-function setCookie(cname, cvalue, exdays) {
-    const d = new Date();
-    d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    let expires = "expires="+ d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-}
 
-// Delete cookie
-function deleteCookie(cname) {
-    const d = new Date();
-    d.setTime(d.getTime() + (24*60*60*1000));
-    let expires = "expires="+ d.toUTCString();
-    document.cookie = cname + "=;" + expires + ";path=/";
-}
 
-// Read cookie
-function getCookie(cname) {
-    let name = cname + "=";
-    let decodedCookie = decodeURIComponent(document.cookie);
-    let ca = decodedCookie.split(';');
-    for(let i = 0; i <ca.length; i++) {
-        let c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
-    }
-    return "";
-}
-
-// Set cookie consent
-function acceptCookieConsent(){
-    deleteCookie('user_cookie_consent');
-    setCookie('user_cookie_consent', 1, 30);
-    document.getElementById("startModal").style.display = "none";
-}
-
-let cookie_consent = getCookie("user_cookie_consent");
-if(cookie_consent != ""){
-    document.getElementById("startModal").style.display = "none";
-}else{
-    document.getElementById("startModal").style.display = "block";
-}
 
 
 
