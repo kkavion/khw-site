969c4b1f3bf84f0fa1971c72f447d3cc5898c9cb
Eric Prem Raju <58141370+ericpremraju@users.noreply.github.com>
2023-11-09 18:03:41 +0530

    moar minor fixes

--- js/lightslider.js
+++ js/lightslider.js
@@ -11,7 +11,7 @@
         cssEasing: 'ease', //'cubic-bezier(0.25, 0, 0.25, 1)',
         easing: 'linear', //'for jquery animation',//
         speed: 500, //ms'
-        auto: false,
+        auto: true,
         pauseOnHover: true,
         loop: true,
         slideEndAnimation: true,
